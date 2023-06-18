bc32580624366a171bb54ab48e52860f32475bc3
Juan Pablo Quiroz Gonzalez <juan.quiroz@proteccion.com.co>
2023-06-17 19:59:16 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { dbConnection } = require('./database/config');
+const { cors } = require('cors');
+require('dotenv').config();
+
+
+// Crear el servidor de express
+
+const app = express(); 
+
+dbConnection();
+
+
+//Directorio publico
+app.use(express.static('public'));
+
+//Lectura y parseo del body
+app.use(express.json());
+
+//Rutas
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+// TODO: CRUD: Eventos
+
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
+})
---
+++ routes/auth.js
@@ -0,0 +1,38 @@
+/*
+    Rutas de usuarios /Auth
+    host + /api/auth
+*/
+
+const { Router } = require('express');
+const router = Router();
+const { crearUsuario, loginUsuario, renovarToken,  } = require('../controllers/auth');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+
+
+router.post(
+    '/register', 
+    [ //Middlewares
+         check('name','El nombre es obligatorio').not().isEmpty(),
+         check('email','El email es obligatorio').not().isEmpty(),
+         check('password','El password debe de ser de 6 caracteres').isLength({min: 6}),
+         validarCampos
+    ],
+    crearUsuario
+);
+
+router.post(
+    '/',
+    [ //Middlewares
+        check('email','El email es obligatorio').not().isEmpty(),
+        check('password','El password debe de ser de 6 caracteres').isLength({min: 6}),
+        validarCampos
+    ],
+    loginUsuario
+);
+
+router.get('/renewtoken', validarJWT, renovarToken);
+
+module.exports = router;
